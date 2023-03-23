f2c0cb43862a8c33c0e8afe3dab65b7ef778be6b
IkerJ14 <ikerjimenezh2@gmail.com>
2023-03-23 19:49:28 +0100

    SDI-2223-307-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 
 var app = express();
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 require("./routes/songs.js")(app)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
